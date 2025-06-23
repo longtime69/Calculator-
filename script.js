0c6cb65ca03dbc15776cf13951600725c853d787
longtime69 <dcor26@icloud.com>
2025-06-23 15:01:19 -0400

    arithmetic function

---
+++ script.js
@@ -0,0 +1,20 @@
+function add(x, y){
+    return x + y;
+}
+
+function subtract(x, y){
+    return x - y;
+}
+
+function multiply(x, y){
+    return x * y;
+}
+
+function divide(x, y){
+    return x / y;
+}
+
+console.log(add(2, 2))
+console.log(subtract(4, 2))
+console.log(multiply(2, 2))
+console.log(divide(6, 2))
