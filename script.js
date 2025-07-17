a7c28d7c888dccc52566a113e504d793c26730fc
longtime69 <dcor26@icloud.com>
2025-07-17 01:02:55 -0400

    operate

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const digit = document.querySelector("#digits")
 const display = document.querySelector("#display")
+const operators = document.querySelector("#operators")
+const Operate = document.querySelector("#operate")
+const clear = document.querySelector("#Clear")
 let number;
 
 function add(x, y){
@@ -22,12 +25,64 @@ function operate(x, y, operation){
     return operation(x, y);
 }
 
-digit.addEventListener('click', (event) => {
-    if(event.target.tagName === 'BUTTON'){
-        display.innerHTML = event.target.innerText;
-        number = event.target.innerText;
-    }
+function calculate(){
+    let firstNumber = '';
+    let secondNumber = '';
+    let operator = null;
+
+    digit.addEventListener('click', (event) => {
+        if(event.target.className === 'digit'){
+            
+                firstNumber += event.target.innerText;
+                display.innerHTML = firstNumber;
+            
+        }
+        
+
+        if(event.target.className === 'digit' && firstNumber != null && operator != null){
+            
+                secondNumber += event.target.innerText;
+                display.innerHTML = secondNumber;
+            
+        }
+
+    
+    })
+
+    operators.addEventListener('click', (event) => {
+        if(event.target.className === 'operator' && firstNumber != '') {
+                operator = event.target.innerText;
+        }
+
+        
+    })
+
+    Operate.addEventListener('click', (event) => {
+        if(event.target.textContent === 'Operate'){
+            let operatorF;
+            if (operator === 'Add') operatorF = add;
+            if (operator === 'Subtract') operatorF = subtract;
+            if (operator === 'Multiply') operatorF = multiply;
+            if (operator === 'Divide') operatorF = divide;
+            let result = operate(Number(firstNumber), Number(secondNumber), operatorF);
+            display.innerHTML = result;
+
+            console.log(result)
+
+            
+        }
+    })
+
+
+   
+
+}
+
+
+
+clear.addEventListener('click', () => {
+    
 })
 
 
-let result = operate(5, 10, add)
+calculate();
