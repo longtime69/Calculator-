d3ae9ceee268898b0077a91f47c6ef54fdedad3f
longtime69 <dcor26@icloud.com>
2025-07-17 15:53:24 -0400

    clear functionality

--- script.js
+++ script.js
@@ -30,8 +30,8 @@ function calculate(){
     let secondNumber = '';
     let operator = null;
     let isSecondNumber = false;
-
-    digit.addEventListener('click', (event) => {
+    
+        digit.addEventListener('click', (event) => {
         if(event.target.className === 'digit'){
             if (!isSecondNumber){
                 firstNumber += event.target.innerText;
@@ -41,43 +41,52 @@ function calculate(){
                 display.innerHTML = secondNumber;
             }
         }
-    })
+        })
 
-    operators.addEventListener('click', (event) => {
-        if(event.target.className === 'operator' && firstNumber !== '') {
-                operator = event.target.innerText;
-                isSecondNumber = true;
-        }
-
-        
-    })
-
-    Operate.addEventListener('click', (event) => {
-        if(event.target.textContent === 'Operate'){
-            let operatorF;
-            if (operator === 'Add') operatorF = add;
-            if (operator === 'Subtract') operatorF = subtract;
-            if (operator === 'Multiply') operatorF = multiply;
-            if (operator === 'Divide') operatorF = divide;
-            let result = operate(Number(firstNumber), Number(secondNumber), operatorF);
-            display.innerHTML = result;
-
-            console.log(result)
+        operators.addEventListener('click', (event) => {
+            if(event.target.className === 'operator' && firstNumber !== '') {
+                    operator = event.target.innerText;
+                    isSecondNumber = true;
+            }
 
             
+        })
+
+        Operate.addEventListener('click', (event) => {
+            if(event.target.textContent === 'Operate'){
+                let operatorF;
+                if (operator === 'Add') operatorF = add;
+                if (operator === 'Subtract') operatorF = subtract;
+                if (operator === 'Multiply') operatorF = multiply;
+                if (operator === 'Divide') operatorF = divide;
+                let result = operate(Number(firstNumber), Number(secondNumber), operatorF);
+                display.innerHTML = result;
+
+                console.log(result)
+
+                firstNumber = '';
+                secondNumber = '';
+                operator = null;
+                isSecondNumber = false;
+            }
+        })
+
+        clear.addEventListener('click', (event) => {
+        if(event.target.textContent === 'Clear'){
+            firstNumber = '';
+            secondNumber = '';
+            operator = null;
+            isSecondNumber = false;
+            display.innerHTML = '0';
         }
-    })
-
-
-   
+        })
+    
 
 }
 
 
 
-clear.addEventListener('click', () => {
-    
-})
+
 
 
 calculate();
